17dfddb9754c550e4e17b98cf69f2927896ac144
Damira Ibragimova <idamira@Damiras-MBP.home>
2016-10-10 10:04:34 -0400

    Made things disappear onclick

--- jquery.js
+++ jquery.js
@@ -1,7 +1,21 @@
 console.log("I'm added!");
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
 
+var objectsToHide = ["butterfly_1", "window", "mouse", "fork"];
 
+
+$(document).ready(function () {
+    $("img").click(function () {
+        if ($.inArray(this.className, objectsToHide) != -1) {
+            $(this).slideUp(1000);
+            // this.style.visibility = "hidden";
+
+            $("li." + this.className).css("text-decoration", "line-through");
+        }
+
+    });
 });
+
+// });
